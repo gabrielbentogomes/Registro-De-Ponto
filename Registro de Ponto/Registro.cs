4f0bc69140414f52747180cba8ba6245aa6769a0
Gabriel <gabrielbgwork@gmail.com>
2023-06-23 02:05:27 -0300

    adicionando as funções de registrar entradas e saídas

--- Registro de Ponto/Registro.cs
+++ Registro de Ponto/Registro.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 using System.Data.SqlClient;
+using iText.StyledXmlParser.Jsoup.Nodes;
 
 namespace Registro_de_Ponto
 {
@@ -48,7 +49,66 @@ namespace Registro_de_Ponto
 
         private void btnRegistrar_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Horário salvo com sucesso!", "Mensagem", MessageBoxButtons.OK);            
+
+            DateTime dtaHora = DateTime.Now;
+            DateTime dt =  DateTime.Now.Date;
+            FuncaoPegarUser f1 = new FuncaoPegarUser();
+            // Obtendo as partes individuais
+            string data = dt.ToString("dd/MM/yyyy");
+            string hora = dtaHora.ToString("HH:mm");
+            if (radioButton1.Checked)
+            {
+                try
+                {
+
+                    SqlConnection con = new SqlConnection();
+                    con.ConnectionString = "Data Source=gabriel261020.database.windows.net;Initial Catalog=Registro_Ponto;User ID=gabrielbento;Password=BDlg@#$!";
+                    con.Open();
+
+                    string insertQuery = "INSERT INTO Entrada VALUES (@Data, @MatriculaFunc, @HoraEntrada);";
+
+                    using (SqlCommand cmd = new SqlCommand(insertQuery, con))
+                    {
+                        cmd.Parameters.AddWithValue("@Data", data);
+                        cmd.Parameters.AddWithValue("@MatriculaFunc", f1.BuscarInformacoesUsuario(matriculas.Matriculas).Matricula.ToString());
+                        cmd.Parameters.AddWithValue("@HoraEntrada", hora);
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Registrado com sucesso");
+                    }
+
+                    con.Close();
+                }
+                catch(Exception ex) {
+                    MessageBox.Show("Ocorreu um erro durante o registro: " + ex.Message);
+                }
+            }
+            else if (radioButton2.Checked)
+            {
+                try
+                {
+
+                    SqlConnection con = new SqlConnection();
+                    con.ConnectionString = "Data Source=gabriel261020.database.windows.net;Initial Catalog=Registro_Ponto;User ID=gabrielbento;Password=BDlg@#$!";
+                    con.Open();
+
+                    string insertQuery = "INSERT INTO Saida VALUES (@Data, @MatriculaFunc, @HoraSaida);";
+
+                    using (SqlCommand cmd = new SqlCommand(insertQuery, con))
+                    {
+                        cmd.Parameters.AddWithValue("@Data", data);
+                        cmd.Parameters.AddWithValue("@MatriculaFunc", f1.BuscarInformacoesUsuario(matriculas.Matriculas).Matricula.ToString());
+                        cmd.Parameters.AddWithValue("@HoraSaida", hora);
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Registrado com sucesso");
+                    }
+
+                    con.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ocorreu um erro durante o registro: " + ex.Message);
+                }
+            }
             this.Close();
         }
 
